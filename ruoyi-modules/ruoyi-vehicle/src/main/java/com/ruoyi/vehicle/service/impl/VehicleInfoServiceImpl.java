12a748be70548ec6c67b1a92ff6740b82f1ec6ac
张奥 <aozhang.chn@gmail.com>
2026-06-11 08:52:12 +0800

    fix: bug修改

--- ruoyi-modules/ruoyi-vehicle/src/main/java/com/ruoyi/vehicle/service/impl/VehicleInfoServiceImpl.java
+++ ruoyi-modules/ruoyi-vehicle/src/main/java/com/ruoyi/vehicle/service/impl/VehicleInfoServiceImpl.java
@@ -181,35 +181,36 @@ public class VehicleInfoServiceImpl implements IVehicleInfoService {
      */
     @Override
     public List<VehicleInfo> selectVehicleInfoList(VehicleInfo vehicleInfo) {
-        List<String> colors = vehicleInfo.getColors();
-        List<String> secondaryColors = vehicleInfo.getSecondaryColors();
         List<SysDictData> colorDict = remoteDictService.getDictDataByType("color").getData();
         Map<String, String> colorMap = colorDict.stream().collect(Collectors.toMap(
                 SysDictData::getDictValue,
                 SysDictData::getDictLabel,
                 (oldVal, newVal) -> oldVal
         ));
-        Set<String> targetLabels = colors.stream()
-                .map(colorMap::get)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toSet());
-
-        Set<String> targetSecondaryLabels = secondaryColors.stream()
-                .map(colorMap::get)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toSet());
-
-        colors = colorMap.entrySet().stream()
-                .filter(entry -> targetLabels.contains(entry.getValue()))
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-
-        secondaryColors = colorMap.entrySet().stream()
-                .filter(entry -> targetSecondaryLabels.contains(entry.getValue()))
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-        vehicleInfo.setColors(colors);
-        vehicleInfo.setSecondaryColors(secondaryColors);
+        if (vehicleInfo.getColors() != null && !vehicleInfo.getColors().isEmpty()) {
+            List<String> colors = vehicleInfo.getColors();
+            Set<String> targetLabels = colors.stream()
+                    .map(colorMap::get)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toSet());
+            colors = colorMap.entrySet().stream()
+                    .filter(entry -> targetLabels.contains(entry.getValue()))
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList());
+            vehicleInfo.setColors(colors);
+        }
+        if (vehicleInfo.getSecondaryColors() != null && !vehicleInfo.getSecondaryColors().isEmpty()) {
+            List<String> secondaryColors = vehicleInfo.getSecondaryColors();
+            Set<String> targetSecondaryLabels = secondaryColors.stream()
+                    .map(colorMap::get)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toSet());
+            secondaryColors = colorMap.entrySet().stream()
+                    .filter(entry -> targetSecondaryLabels.contains(entry.getValue()))
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList());
+            vehicleInfo.setSecondaryColors(secondaryColors);
+        }
         List<VehicleInfo> list = vehicleInfoMapper.selectVehicleInfoList(vehicleInfo);
         for (VehicleInfo vehicle : list) {
             if (StringUtils.isNotBlank(vehicle.getJson())) {
